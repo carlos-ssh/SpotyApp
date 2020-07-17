b92025a965432aec1b13ace71a9fb00792dc340f
Carlos Robles <carlos.anezeh@gmail.com>
2020-07-17 14:47:58 -0500

    take id for every artist and created the artist component

--- src/app/components/artista/artista.component.ts
+++ src/app/components/artista/artista.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-artista',
   templateUrl: './artista.component.html',
   styleUrls: ['./artista.component.css']
 })
-export class ArtistaComponent implements OnInit {
+export class ArtistaComponent {
 
-  constructor() { }
+  constructor( private router: ActivatedRoute ) { 
 
-  ngOnInit(): void {
+    this.router.params.subscribe( params => {
+      console.log(params['id']);
+    })
   }
-
-}
+} 
--- src/app/components/tarjetas/tarjetas.component.ts
+++ src/app/components/tarjetas/tarjetas.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tarjetas',
@@ -9,8 +11,20 @@ export class TarjetasComponent {
 
   @Input() items: any[] = [];
 
-  constructor() { }
+  constructor( private router: Router ) { }
 
+  verArtista( item: any ) {
 
+    let artistaId;
+
+    if( item.type === 'artist' ){
+      artistaId = item.id;
+    } else {
+       artistaId = item.artists[0].id;
+    }
+     
+    this.router.navigate([ '/artist', artistaId ]);
+
+  }
 
 }
